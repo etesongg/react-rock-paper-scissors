5af4a4041845cc033c7cc4539174053e7b5d73b6
etesong014 <etesong0114@gmail.com>
2024-08-13 03:22:52 +0900

    승패 결과 이미지 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Button } from 'react-bootstrap';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Box from "./component/Box";
+import ResultImgBox from "./component/ResultImgBox"
 
 // 1. 유저는 박스 두개를 볼 수 있다.(타이틀, 사진, 결과).
 // 2. 유저는 박스 하단에 가위바위보 버튼을 볼 수 있다.
@@ -26,17 +27,42 @@ const choice = {
   },
 };
 
+const resultImages = {
+  Default: {
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgWBmlC8AhsYmHJyJ4wDnO5ZnvioVxpQz9R8_09y2IDiCDeiilKF2eqnC459E3Z4ZeydO35MjWgVstMseI9joqQjBjNl4bFS4-6NpAZLj8cfwzpdHxqBf8wm-rJ2jsAbErM4VNq91NOhpg/s400/animal_dance_rabbit.png"
+  },
+  Win: {
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgx-hG3cv1inc_RWkGIXVwcCwhY0urLWRBjwrmGFGhHHSRFLZLHFRqmLyG2E3j6PTRIcIU0qJu3T-QjVlqAdIPiEficf65fDtZSU4Z8DGfVpuBQ9wQ_7V66zuNStZWybQZYmwWgsZbA6do/s300/douzo_usagi_left.png"
+  },
+  Lose: {
+    img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEih-pjgVz3CJR0yaxmMZsKiopMwl1rarbmptXCLWd5l3K6pOC4nXlf1B02Psu0dyiS-vkOKZpYCQZ3vHz9l1KCptd2QMcSvJQ8UHbwAhfEwuew-oVbn70ExWl0lGKTYXDo0qPvUzYfLZTM/s300/douzo_usagi_right.png"
+  }
+}
+
 function App() {
   const [userSelect, setUserSelect] = useState(null);
   const [computerSelect, setComputerSelect] = useState(null);
   const [result, setResult] = useState("");
+  const [resultImage, setResultImage] = useState(resultImages.Default);
 
   const play = (userChoice) => {
     setUserSelect(choice[userChoice]);
     let computerChoice = randomChoice();
     setComputerSelect(computerChoice);
-    setResult(judgement(choice[userChoice], computerChoice));
+    let judgementResult = judgement(choice[userChoice], computerChoice);
+    setResult(judgementResult)
+    setResultImage(getResultImage(judgementResult))
   };
+
+  const getResultImage = (my_result) => {
+    if(my_result === "Win") {
+      return resultImages.Win;
+    } else if (my_result === "Lose"){
+      return resultImages.Lose;
+    }else{
+      return resultImages.Default;
+    }
+  }
 
   const judgement = (user, computer) => {
     if (user.name === computer.name) {
@@ -60,6 +86,7 @@ function App() {
     <div>
       <div className="main">
         <Box title="You" item={userSelect} result={result} />
+        <ResultImgBox img={resultImage.img}/>
         <Box title="Computer" item={computerSelect} result={result} />
       </div>
       <div className="main">
---
+++ src/component/ResultImgBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ResultImgBox = (props) => {
+  return (
+    <div className='box result-img'>
+      <img src={props.img}></img>
+    </div>
+  )
+}
+
+export default ResultImgBox
